f9f038781a02b216c5a0494a5e3400c727f68a24
Ciarán Mooney <admin@cmooney.co.uk>
2025-10-19 11:53:12 +0100

    Handle edge case where REPLACE edits contain original violations

--- diff-pmd/src/main/java/io/github/yangziwen/pmd/filter/DiffLineFilter.java
+++ diff-pmd/src/main/java/io/github/yangziwen/pmd/filter/DiffLineFilter.java
@@ -24,12 +24,20 @@ public class DiffLineFilter {
         if (editList == null || editList.isEmpty()) {
             return false;
         }
-        for (Edit edit : editList) {
-            if (edit.getBeginB() <= violation.getBeginLine() && edit.getEndB() >= violation.getEndLine()) {
-                return true;
-            }
-        }
-        return false;
+        return editList.stream().anyMatch(edit -> {
+            int offset = insertedWithinExistingLines(edit) ? 1 : 0;
+            int changedLineBegin = edit.getBeginB() + offset;
+            int changedLineEnd = edit.getEndB() + offset;
+            return violationWithinEdit(violation, changedLineBegin, changedLineEnd);
+        });
+    }
+
+    private static boolean insertedWithinExistingLines(Edit edit) {
+        return edit.getBeginA() + 1 == edit.getEndA();
+    }
+
+    private static boolean violationWithinEdit(RuleViolation violation, int changedLineBegin, int changedLineEnd) {
+        return changedLineBegin <= violation.getBeginLine() && changedLineEnd >= violation.getEndLine();
     }
 
 }
